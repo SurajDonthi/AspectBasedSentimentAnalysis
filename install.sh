cb9780a250234f4962ac36d224314a611ca7ce9e
SurajDonthi <surajdonthi95@gmail.com>
2021-06-22 05:37:30 +0000

    Updated install instructions

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
+CONDA_ENV=PytorchLightningEnv
 echo "Cleaning up older environment"
-conda env remove -n NLPDev
+conda env remove -n $CONDA_ENV
 conda env create -f environment.yml
 eval "$(conda shell.bash hook)"
-conda activate NLPDev
-echo "Activated NLPDev"
+conda activate $CONDA_ENV
+echo "Activated "$CONDA_ENV
 echo "Installing spaCy models..."
 spacy download en_core_web_md
 spacy download en_core_web_sm
 spacy download en
-echo "Installing neuralcoref..."
-git clone https://github.com/huggingface/neuralcoref.git ~/neuralcoref
-cd ~/neuralcoref/
-pip install -r requirements.txt
-pip install -e .
-cd -
+# echo "Installing neuralcoref..."
+# git clone https://github.com/huggingface/neuralcoref.git ~/neuralcoref
+# cd ~/neuralcoref/
+# pip install -r requirements.txt
+# pip install -e .
+# cd -
 # echo "Deleting neuralcoref folder..."
 # rm -r ./neuralcoref
 echo "Removing unused packages..."
