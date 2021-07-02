260b07db5b81ff0c8b01e706d71dfd315e41d61d
SurajDonthi <surajdonthi95@gmail.com>
2021-07-02 11:39:34 +0530

    Updated powershell setup

--- install.ps1
+++ install.ps1
@@ -1,19 +1,20 @@
+$CONDA_ENV="PytorchLightningEnv"
 echo "Cleaning up older environment..."
-conda env remove -n NLPDev
-echo "Creating environment NLPDev..."
+conda env remove -n $CONDA_ENV
+echo "Creating environment $CONDA_ENV..."
 conda env create -f environment.yml
-conda activate NLPDev
-echo "Activated NLPDev"
+conda activate $CONDA_ENV
+echo "Activated $CONDA_ENV"
 echo "Installing spaCy models..."
 python -m spacy download en_core_web_md
 python -m spacy download en_core_web_sm
 python -m spacy download en
-echo "Installing neuralcoref..."
-git clone https://github.com/huggingface/neuralcoref.git $HOME/neuralcoref
-Push-Location $HOME/neuralcoref/
-pip install -r requirements.txt
-pip install -e .
-Pop-Location
+# echo "Installing neuralcoref..."
+# git clone https://github.com/huggingface/neuralcoref.git $HOME/neuralcoref
+# Push-Location $HOME/neuralcoref/
+# pip install -r requirements.txt
+# pip install -e .
+# Pop-Location
 # echo "Deleting neuralcoref folder..."
 # rm -r ./neuralcoref
 echo "Removing unused packages..."
